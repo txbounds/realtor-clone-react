24e5c9de13136968c04135a6af0ced6c8698dc49
Troy Bounds <troy.bounds@icloud.com>
2022-11-11 13:46:42 +0000

    Create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-2xl bg-slate-600'>Hello Big World 2.00</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
