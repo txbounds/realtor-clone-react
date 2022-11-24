9e394994c5ef27cbbdb5bcf73c8a7ca0ec9bcceb
Troy Bounds <troy.bounds@icloud.com>
2022-11-24 21:51:39 +0000

    Add functionality onsubmit and create listing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,24 +1,45 @@
+import { async } from "@firebase/util";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { v4 as uuidv4 } from "uuid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateListing() {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
-    bedroom: 1,
-    bathroom: 1,
+    bedrooms: 1,
+    bathrooms: 1,
     parking: false,
     furnished: false,
     address: "",
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
+    latitude: 0,
+    longitude: 0,
+    images: {},
   });
 
   const {
     type,
     name,
-    bedroom,
-    bathroom,
+    bedrooms,
+    bathrooms,
     parking,
     furnished,
     address,
@@ -26,12 +47,143 @@ export default function CreateListing() {
     offer,
     regularPrice,
     discountedPrice,
+    latitude,
+    longitude,
+    images,
   } = formData;
-  function onChange() {}
+
+  function onChange(e) {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+    // Files
+
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  }
+  async function onSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    if (+discountedPrice >= +regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price needs to be less than the regular price");
+      return;
+    }
+
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("maximum 6 images are allowed");
+      return;
+    }
+    let geolocation = {};
+    let location;
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location = data.status === "ZERO_RESULTS" && undefined;
+      if (location === undefined) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+    }
+    async function storeImage(image) {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, filename);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            // Handle unsuccessful uploads
+            reject(error);
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    }
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch((error) => {
+      setLoading(false);
+      toast.error("Images not uploaded");
+      return;
+    });
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
+    delete formDataCopy.images;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+    delete formDataCopy.latitude;
+    delete formDataCopy.longitude;
+
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+    setLoading(false);
+    toast.success("Listing created");
+
+    navigate(`/category/${formDataCopy.type}/${docRef}`);
+
+    // console.log(imgUrls);
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <main className='x-w-md px-2 mx-auto'>
       <h1 className='text-3xl text-center mt-6 font-bold'>Create a Listing</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <p className='text-lg mt-6 font-semibold'>Sell / Rent</p>
         <div className='flex'>
           <button
@@ -57,7 +209,7 @@ export default function CreateListing() {
           <button
             type='button'
             id='type'
-            value='sale'
+            value='rent'
             onClick={onChange}
             className={`ml-3 px-7 py-3 
             font-medium text-sm 
@@ -95,7 +247,7 @@ export default function CreateListing() {
             <input
               type='number'
               id='bedrooms'
-              value={bedroom}
+              value={bedrooms}
               onChange={onChange}
               min='1'
               max='50'
@@ -109,7 +261,7 @@ export default function CreateListing() {
             <input
               type='number'
               id='bathrooms'
-              value={bathroom}
+              value={bathrooms}
               onChange={onChange}
               min='1'
               max='50'
@@ -211,6 +363,39 @@ export default function CreateListing() {
             className='w-full px-4 py-2 text-xl text-gray-700
   bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'
           />
+          {!geolocationEnabled && (
+            <div className='flex space-x-6 justify-start mb-6'>
+              <div className=''>
+                <p className='text-lg font-semibold'>Latitude</p>
+                <input
+                  type='number'
+                  id='latitude'
+                  value={latitude}
+                  onChange={onChange}
+                  required
+                  min='-90'
+                  max='90'
+                  className='w-full px-4 py-2 text-xl text-gray-700
+                  bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'
+                />
+              </div>
+
+              <div className=''>
+                <p className='text-lg font-semibold'>Longitude</p>
+                <input
+                  type='number'
+                  id='longitude'
+                  value={longitude}
+                  onChange={onChange}
+                  required
+                  min='-180'
+                  max='180'
+                  className='w-full px-4 py-2 text-xl text-gray-700
+                bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'
+                />
+              </div>
+            </div>
+          )}
 
           <p className='text-lg  font-semibold'>Description</p>
           <textarea
@@ -355,7 +540,7 @@ export default function CreateListing() {
           text-sm 
           uppercase 
           rounded shadow-md
-           hover:bg-blue-700 
+           hover:bg-blue-800 
            hover:shadow-lg focus: bg-blue-700  focus: shadow-lg
            active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out: ;
            '
